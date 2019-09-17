cc9bd3eb2f9383ff152df51a74f49671ccb41425
twhalen <thor@otosense.com>
2019-09-17 09:25:11 -0700

    dispatch_funcs: One-stop-shop function to dispatch a list of functions to cli, webservice, or dash-ui.

---
+++ dispatch_anything.py
@@ -0,0 +1,36 @@
+"""Dispatch anything to anything.
+
+Well... for now, a list of functions to cli, ws, or dash.
+
+"""
+
+
+def dispatch_funcs(funcs, interface=None, *args, **kwargs):
+    if interface is None:
+        print("Choices of interface:")
+        print(", ".format(['cli', 'ws', 'dash']))
+        return
+
+    if not isinstance(funcs, (tuple, list)):
+        funcs = [funcs]
+
+    if interface == 'cli':
+        from argh import dispatch_commands
+
+        dispatch_commands(funcs)
+
+    elif interface == 'ws':
+        from py2api.py2rest.app_maker import dflt_run_app_kwargs, dispatch_funcs_to_web_app
+
+        app = dispatch_funcs_to_web_app(funcs, *args, **kwargs)
+
+        run_app_kwargs = kwargs.get('run_app_kwargs', dflt_run_app_kwargs)
+        app.run(**run_app_kwargs())
+
+    elif interface == 'dash':
+
+        from py2dash.app_makers import dispatch_funcs
+
+        app = dispatch_funcs(funcs)
+        debug = kwargs.get('debug', False)
+        app.run_server(debug=debug)
