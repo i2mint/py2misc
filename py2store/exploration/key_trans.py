22e5a2ecdf36dd2627f8d72c63105b21e190d536
twhalen <thor@otosense.com>
2020-01-27 18:28:29 -0800

    More on mk_tupled_store_from_path_format_store_cls

--- py2store/exploration/key_trans.py
+++ py2store/exploration/key_trans.py
@@ -1,18 +1,18 @@
 import os
+from functools import wraps
 from py2store.key_mappers.naming import StrTupleDict
 from py2store.trans import wrap_kvs
 
 pjoin = os.path.join
 
 
-def mk_tupled_store_from_path_format_store_cls(store, root_uri, subpath, store_cls_kwargs=None,
+def mk_tupled_store_from_path_format_store_cls(store, subpath='', store_cls_kwargs=None,
                                                keymap=StrTupleDict, keymap_kwargs=None,
                                                name=None):
     """Wrap a store (instance or class) that uses string keys to make it into a store that uses tuple keys.
 
     Args:
         store: The instance or class to wrap
-        root_uri: The root uri of the data
         subpath: The subpath (defining the subset of the data pointed at by the URI
         store_cls_kwargs:  # if store is a class, the kwargs that you would have given the store_cls to make itself
         keymap:  # the keymap instance or class you want to use to map keys
@@ -36,10 +36,16 @@ def mk_tupled_store_from_path_format_store_cls(store, root_uri, subpath, store_c
         keymap = keymap(subpath, **(keymap_kwargs or {}))  # make the keymap instance
 
     if isinstance(store, type):
-        name = name or 'StdWrapped' + store.__name__
-        WrappedStoreCls = wrap_kvs(store, name=name,
-                                   key_of_id=keymap.str_to_tuple, id_of_key=keymap.tuple_to_str)
-        return WrappedStoreCls(pjoin(root_uri, subpath), **store_cls_kwargs or {})
+        name = name or 'Str2TupleWrapped' + store.__name__
+        _WrappedStoreCls = wrap_kvs(store, name=name,
+                                    key_of_id=keymap.str_to_tuple, id_of_key=keymap.tuple_to_str)
+
+        class WrappedStoreCls(_WrappedStoreCls):
+            def __init__(self, root_uri):
+                path_format = pjoin(root_uri, subpath)
+                super().__init__(path_format, **(store_cls_kwargs or {}))
+
+        return WrappedStoreCls
     else:
-        name = name or 'StdWrapped' + store.__class__.__name__
+        name = name or 'Str2TupleWrapped' + store.__class__.__name__
         return wrap_kvs(store, name=name, key_of_id=keymap.str_to_tuple, id_of_key=keymap.tuple_to_str)
