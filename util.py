ff0d3c1554d8578806437e26ae64485fc58c39b6
twhalen <thor@otosense.com>
2020-03-23 16:09:04 -0400

    SubscriptableType

--- util.py
+++ util.py
@@ -26,3 +26,22 @@ It seems you don't have requred `{exc_val.name}` package. Not sure if this is th
     git clone https://github.com/i2mint/{exc_val.name}
 in a place that your python path (i.e. PYTHONPATH environment variable).  
             """)
+
+
+class SubscriptableType:
+    """
+    To make type ints that are subscriptable.
+    If you're custom type is a contaiiner of sorts, you might want to say something about the type of it's contents.
+    This is a way. Using the dunder: __class_getitem__
+
+    >>> class A(SubscriptableType): ...
+    >>> def f(a: A[str], b: A[int, float], c: int): ...
+    >>> f.__annotations__
+    {'a': 'util.A[str]', 'b': 'util.A[int, float]', 'c': <class 'int'>}
+    """
+
+    def __class_getitem__(cls, item):
+        if not isinstance(item, tuple):
+            return f"{cls.__module__}.{cls.__qualname__}[{item.__name__}]"
+        else:
+            return f"{cls.__module__}.{cls.__qualname__}[{', '.join([x.__name__ for x in item])}]"
